00000467837a3108c635f491f8372852da665409
maaz bin Hassan <mleghari64@gmail.com>
2023-10-09 16:08:52 +0500

    updated admin files to gooffunctionality

--- js/admin.js
+++ js/admin.js
@@ -1,30 +1,61 @@
-// admin.js
+// Sample hardcoded password (for demonstration purposes)
+const adminPassword = "password123";
+
+// Menu items array (for demonstration purposes)
+let menuItems = ["Coffee", "Tea", "Croissant", "Muffin", "Sandwich"];
 
 document.addEventListener("DOMContentLoaded", function () {
-    const passwordForm = document.getElementById("passwordForm");
-    const usernameInput = document.getElementById("username");
-    const passwordInput = document.getElementById("password");
+    const loginForm = document.getElementById("loginForm");
     const adminContent = document.getElementById("adminContent");
+    const passwordInput = document.getElementById("password");
+    const loginBtn = document.getElementById("loginBtn");
+    const menuList = document.getElementById("menuList");
+    const addItemBtn = document.getElementById("addItemBtn");
+    const deleteItemBtn = document.getElementById("deleteItemBtn");
 
-    passwordForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-
-        const enteredUsername = usernameInput.value;
+    loginBtn.addEventListener("click", function () {
         const enteredPassword = passwordInput.value;
-
-        // Replace these with your actual username and password
-        const validUsername = "admin";
-        const validPassword = "admin123";
-
-        if (enteredUsername === validUsername && enteredPassword === validPassword) {
-            // Correct username and password, show admin content
-            passwordForm.style.display = "none";
+        if (enteredPassword === adminPassword) {
+            // Correct password, show admin content
+            loginForm.style.display = "none";
             adminContent.style.display = "block";
+            displayMenuItems();
         } else {
-            // Incorrect username or password, display an error message
-            alert("Incorrect username or password. Please try again.");
-            usernameInput.value = "";
+            // Incorrect password, display an error message
+            alert("Incorrect password. Please try again.");
             passwordInput.value = "";
         }
     });
+
+    addItemBtn.addEventListener("click", function () {
+        const newItem = prompt("Enter a new menu item:");
+        if (newItem) {
+            menuItems.push(newItem);
+            displayMenuItems();
+        }
+    });
+
+    deleteItemBtn.addEventListener("click", function () {
+        const selectedItemIndex = menuList.selectedIndex;
+        if (selectedItemIndex >= 0) {
+            menuItems.splice(selectedItemIndex, 1);
+            displayMenuItems();
+        } else {
+            alert("Please select a menu item to delete.");
+        }
+    });
+
+    menuList.addEventListener("change", function () {
+        deleteItemBtn.disabled = menuList.selectedIndex === -1;
+    });
+
+    function displayMenuItems() {
+        menuList.innerHTML = "";
+        menuItems.forEach((item, index) => {
+            const option = document.createElement("option");
+            option.textContent = item;
+            menuList.appendChild(option);
+        });
+        deleteItemBtn.disabled = true;
+    }
 });
