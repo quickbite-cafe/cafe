0eb2ee51152edaaa9366e069ea786568da9b7d0c
maaz bin Hassan <mleghari64@gmail.com>
2023-10-09 15:57:17 +0500

    added landing page with all the structure and files with css javascript and index html code and images

---
+++ js/main.js
@@ -0,0 +1,118 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Fixed Navbar
+    $('.fixed-top').css('top', $('.top-bar').height());
+    $(window).scroll(function () {
+        if ($(this).scrollTop()) {
+            $('.fixed-top').addClass('bg-dark').css('top', 0);
+        } else {
+            $('.fixed-top').removeClass('bg-dark').css('top', $('.top-bar').height());
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1500,
+        loop: true,
+        nav: true,
+        dots: false,
+        items: 1,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+    
+})(jQuery);
+// admin.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const passwordForm = document.getElementById("passwordForm");
+    const passwordInput = document.getElementById("password");
+    const adminContent = document.getElementById("adminContent");
+
+    passwordForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const enteredPassword = passwordInput.value;
+        const adminPassword = "admin123"; // Replace with your actual admin password
+
+        if (enteredPassword === adminPassword) {
+            // Correct password, show admin content
+            passwordForm.style.display = "none";
+            adminContent.style.display = "block";
+        } else {
+            // Incorrect password, display an error message
+            alert("Incorrect password. Please try again.");
+            passwordInput.value = "";
+        }
+    });
+});
+
